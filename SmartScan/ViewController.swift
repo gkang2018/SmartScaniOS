6dee1c95155e803083f50d822b526fdaab20ea28
Joshua Diamond <joshuadiamond@dhcp-wifi-8021x-155-41-101-230.bu.edu>
2020-02-08 13:10:36 -0500

    Initial commit

---
+++ SmartScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartScan
+//
+//  Created by Joshua Diamond on 2/8/20.
+//  Copyright © 2020 Gagandeep Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
